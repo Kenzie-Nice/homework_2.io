8ccc682705949c13461ea531fc61c4b3c1143bff
Kenzie-Nice <157401036+Kenzie-Nice@users.noreply.github.com>
2025-01-28 09:21:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// Logging favorite websites
+
 console.log("Favorite Websites:");
 console.log("1. https://www.youtube.com/ ");
 console.log("2. https://www.etsy.com/?ref=lgo/ I have issues....");
 console.log("3. https://www.crunchyroll.com/");
 
-// Logging favorite games
+
 console.log("Favorite Games:");
 console.log("1.Final Fantasy XIV");
 console.log("2. Devil May Cry");
 console.log("3. Ghost of Tsushima");
 
-// Logging favorite artists and their cultural importance
+
 console.log("Favorite Artists:");
 console.log("1. Two Steps From Hell");
 console.log("2. A1-Studios");
 console.log("3. Viva la Dirt League");
 
-// Asking a question using prompt
+
 const favoriteFood = prompt("What's your favorite food?");
 console.log(`Your favorite food is: ${Food}`);
